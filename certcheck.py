1a61048b4520350b9e3d17d2ffe1c06aa0ef6017
Tomas Libal <tomas@libal.eu>
2017-02-07 22:08:54 +0800

    use the new runcmd command
    change logging level to INFO

--- certcheck.py
+++ certcheck.py
@@ -4,15 +4,15 @@ from fabric.api import run, env
 from datetime import datetime
 
 
-logging.basicConfig(filename='when-expires.log',level=logging.DEBUG)
+logging.basicConfig(filename='when-expires.log',level=logging.INFO)
 
-def runcmd(cmd, on_success, on_error):
-    logging.debug('Running cmd [%s] %s', env.host, cmd)
-    output = run(cmd)
+def runcmd(cmd, args, on_success, on_error):
+    logging.info('Running cmd [%s] %s', env.host, cmd)
+    output = run(cmd % args)
     if output.succeeded:
-        on_success(output)
+        on_success(output, args)
     else:
-        on_error(output)
+        on_error(output, args)
 
 def get_hosts():
     hosts = []
@@ -36,20 +36,23 @@ def get_certs_to_check(selected_host):
 
     return certs
 
+def check_error(output, args):
+    print "error checking the certificate"
+
+def check_success(output, args):
+    cert_path = args
+    exp = output.split("=")[1]
+    exp = datetime.strptime(exp, '%b %d %H:%M:%S %Y GMT')
+    if exp > datetime.now():
+        valid = (exp - datetime.now()).days
+        print "%s:%s OK (expires in %d days)" % (env.host, cert_path, valid)
+    else:
+        print "%s:%s EXPIRED!" % (env.host, cert_path)
+
 def check_certs(certs):
     for cert_path in certs:
-        print "checking %s" % (cert_path)
-        o = run('openssl x509 -enddate -noout -in ' + cert_path)
-        if not o.succeeded:
-            print "error checking..."
-        else:
-            exp = o.split("=")[1]
-            exp = datetime.strptime(exp, '%b %d %H:%M:%S %Y GMT')
-            if exp > datetime.now():
-                valid = (exp - datetime.now()).days
-                print "%s:%s OK (expires in %d days)" % (env.host, cert_path, valid)
-            else:
-                print "expired!!"
+        logging.info("checking %s", cert_path)
+        runcmd('openssl x509 -enddate -noout -in %s', (cert_path), check_success, check_error)        
 
 def check_on_web(urls):
     port = 443
