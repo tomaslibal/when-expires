c17b8fa0758be014f9363cddd08e431fbb3c018b
Tomas Libal <tomas@libal.eu>
2017-02-07 23:09:13 +0800

    explicitly state what is a task

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,16 @@
-from fabric.api import run, env, hosts
+from fabric.api import run, env, hosts, task
 from certcheck import get_hosts, get_certs_to_check, check_certs, get_weblist, check_on_web
 
 env.hosts = get_hosts()
 
+@task
 def check_cert():
     current_host = env.host
     print "checking on %s " % (current_host)
     certs_to_check = get_certs_to_check(current_host)
     check_certs(certs_to_check)
 
+@task
 @hosts('localhost')
 def check_web():
     web_certs = get_weblist()
